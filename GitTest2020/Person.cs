be6da46b39d195d16e1559d478546d0ee327bfef
mads81i7 <73933098+mads81i7@users.noreply.github.com>
2020-11-27 11:03:53 +0100

    Update Person.cs

--- GitTest2020/Person.cs
+++ GitTest2020/Person.cs
@@ -7,10 +7,12 @@ namespace GitTest2020
     public class Person
     {
         public string Name { get; set; }
+        public int Id {get; set; }
 
-        public Person(string name)
+        public Person(string name, int id)
         {
             Name = name;
+            Id = id
         }
 
         public override string ToString()
