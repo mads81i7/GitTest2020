ee76a00b68486421bda21e99f0547f7324f3cf9c
mads81i7 <73933098+mads81i7@users.noreply.github.com>
2020-11-27 11:23:27 +0100

    Update Person.cs
    Added address to person

--- GitTest2020/Person.cs
+++ GitTest2020/Person.cs
@@ -9,12 +9,14 @@ namespace GitTest2020
         public string Name { get; set; }
         public int Id {get; set; }
         public string PhoneNo { get; set; }
+        public string Address { get; set; }
 
-        public Person(string name, int id, string phoneNo)
+        public Person(string name, int id, string phoneNo, string address)
         {
             Name = name;
             Id = id;
             PhoneNo = phoneNo;
+            Address = address;
         }
 
         public override string ToString()
